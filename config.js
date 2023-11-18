8c90e410070bd2b109323d375d749b0a7cb5a6c9
FEROZMEMON <ferozmemon726@gmail.com>
2023-11-18 12:52:13 +0500

    login done

---
+++ config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIISJ1LfRtiKFlx--IixN1h7hiJVK-Tdk",
+  authDomain: "todo-list-726.firebaseapp.com",
+  projectId: "todo-list-726",
+  storageBucket: "todo-list-726.appspot.com",
+  messagingSenderId: "806539189216",
+  appId: "1:806539189216:web:55d2b7be435fc1b6152985",
+  measurementId: "G-L0CHHTRJGT"
+};
+
+const app = initializeApp(firebaseConfig);
